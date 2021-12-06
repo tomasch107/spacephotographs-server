5e0c8b351f547167bcce0c53c6851e962b48edba
Tomek Dworak <tomasch107@gmail.com>
2021-12-06 19:40:58 +0100

    initial commit

---
+++ src/main/java/com/spacephotographs/SpacephotographsApplication.java
@@ -0,0 +1,13 @@
+package com.spacephotographs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpacephotographsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpacephotographsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/spacephotographs/SpacephotographsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spacephotographs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpacephotographsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
